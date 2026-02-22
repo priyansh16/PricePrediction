64f9a452713cd2016fdd7fae3511efb86fd51815
Priyansh Gupta <49022779+priyansh16@users.noreply.github.com>
2026-02-22 13:51:35 +0100

    add monitoring dashboard to the application
    Updated app.py with monitoring API.
    Created monitoring page.

--- app.py
+++ app.py
@@ -32,30 +32,84 @@ def predict():
         
         prediction =  pipeline.predict(df)[0]
         
+        #Logging
         log_data = df.copy()
-        log_data["prediction"] = round(prediction, 2)
-        log_data["timestamp"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_data["Prediction"] = round(prediction, 2)
+        log_data["Timestamp"] = datetime.datetime.now()
         
         log_file = os.path.join(BASE_DIR, "model_logs.csv")
         # Append to CSV, only write header if file doesn't exist
-        log_data.to_csv(log_file, mode='a', index=False, header=not os.path.exists(log_file))
+        log_data.to_csv(
+            log_file, 
+            mode='a',
+            index=False,
+            header=not os.path.exists(log_file),
+            encoding="utf-8"
+            )
         
         return jsonify({"value": round(prediction, 2)})
     except Exception as e:
         return jsonify({"error": str(e)})
     
     
-@app.route("/logs")
-def view_logs():
-    try:
-        log_file = os.path.join(BASE_DIR, "model_logs.csv")
-        if os.path.exists(log_file):
-            df_logs = pd.read_csv(log_file)
-            # Return as HTML table for a quick "dashboard"
-            return df_logs.tail(20).to_html(classes='table table-striped')
-        return "No logs found yet."
-    except Exception as e:
-        return str(e)
+# ------------------------
+# Monitoring Dashboard Page
+# ------------------------
+
+@app.route("/monitoring")
+def monitoring():
+    return render_template("monitoring.html")
+
+
+@app.route("/monitoring-data")
+def monitoring_data():
+
+    log_file = os.path.join(BASE_DIR, "model_logs.csv")
+
+    if not os.path.exists(log_file):
+        return jsonify({})
+
+    df = pd.read_csv(log_file, encoding="utf-8")
+    
+    if df.empty:
+        print('empty file')
+        return jsonify({})
+
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"], errors="coerce")
+    df["date"] = df["Timestamp"].dt.date
+
+    # Prediction count per day
+    usage = df.groupby("date").size()
+
+    # Average predicted price by date and house type
+    avg_by_type = (df.groupby(["date", "House_type"])["Prediction"].mean().reset_index())
+    
+    pivot_avg = avg_by_type.pivot(
+        index = "date",
+        columns = "House_type",
+        values = "Prediction"
+    ).fillna(method="ffill")
+    
+    pivot_avg = pivot_avg.sort_index()
+    
+    # Municipality distribution
+    municipality_counts = df["Municipality"].value_counts()
+    print(f"usage_dates: {usage.index.astype(str).tolist()}, usage_counts: {usage.tolist()}, avg_dates:{pivot_avg.index.astype(str).tolist()}")
+    print(f"avg_values: {pivot_avg.to_dict(orient = "list")}, municipalities: {municipality_counts.index.tolist()}, municipality_counts: {municipality_counts.tolist()}")
+    print(f"recent_logs: {df.tail(5).to_dict(orient="records")}")
+    
+    return jsonify({
+        "usage_dates": usage.index.astype(str).tolist(),
+        "usage_counts": usage.tolist(),
+        
+        "avg_dates": pivot_avg.index.astype(str).tolist(),
+        "avg_values": pivot_avg.to_dict(orient = "list"),
+        
+        "municipalities": municipality_counts.index.tolist(),
+        "municipality_counts": municipality_counts.tolist(),
+        "recent_logs": df.tail(5).to_dict(orient="records")
+    })
+
 
 if __name__ == "__main__":
     app.debug = True
